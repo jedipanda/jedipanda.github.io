0dbb5ee7c334da07928dd7ff53a4663c34188f87
jedipanda <andreidolca@gmail.com>
2020-05-29 14:56:06 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -253,14 +253,17 @@ function toggleCheck3(){
   var x = document.getElementById("iframez3");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink3');
+  var z3 = document.getElementById("activezoom3");
   
   if(document.getElementById("myCheckbox3").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z3.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z2.style.color = "gray";
     
   }
 }
@@ -271,14 +274,17 @@ function toggleCheck4(){
   var x = document.getElementById("iframez4");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink4');
+  var z4 = document.getElementById("activezoom4");
   
   if(document.getElementById("myCheckbox4").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z4.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z4.style.color = "gray";
     
   }
 }
@@ -289,15 +295,17 @@ function toggleCheck5(){
   var x = document.getElementById("iframez5");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink5');
+  var z5 = document.getElementById("activezoom5");
   
   if(document.getElementById("myCheckbox5").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z5.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
-    
+    z5.style.color = "gray";
   }
 }
 
@@ -307,14 +315,17 @@ function toggleCheck6(){
   var x = document.getElementById("iframez6");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink6');
+  var z6 = document.getElementById("activezoom6");
   
   if(document.getElementById("myCheckbox6").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z6.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z6.style.color = "gray";
     
   }
 }
@@ -325,14 +336,17 @@ function toggleCheck7(){
   var x = document.getElementById("iframez7");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink7');
+  var z7 = document.getElementById("activezoom7");
   
   if(document.getElementById("myCheckbox7").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z7.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z7.style.color = "gray";
     
   }
 }
@@ -343,14 +357,17 @@ function toggleCheck8(){
   var x = document.getElementById("iframez8");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink8');
+  var z8 = document.getElementById("activezoom8");
   
   if(document.getElementById("myCheckbox8").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z8.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z8.style.color = "gray";
     
   }
 }
@@ -361,14 +378,17 @@ function toggleCheck9(){
   var x = document.getElementById("iframez9");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink9');
+  var z9 = document.getElementById("activezoom9");
   
   if(document.getElementById("myCheckbox9").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z9.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z9.style.color = "gray";
     
   }
 }
@@ -379,14 +399,17 @@ function toggleCheck10(){
   var x = document.getElementById("iframez10");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink10');
+  var z10 = document.getElementById("activezoom10");
   
   if(document.getElementById("myCheckbox10").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z10.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z10.style.color = "gray";
     
   }
 }
@@ -397,14 +420,17 @@ function toggleCheck11(){
   var x = document.getElementById("iframez11");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink11');
+  var z11 = document.getElementById("activezoom11");
   
   if(document.getElementById("myCheckbox11").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z11.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z11.style.color = "gray";
     
   }
 }
@@ -415,14 +441,17 @@ function toggleCheck12(){
   var x = document.getElementById("iframez12");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink12');
+  var z12 = document.getElementById("activezoom12");
   
   if(document.getElementById("myCheckbox12").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z12.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z12.style.color = "gray";
     
   }
 }
@@ -433,14 +462,17 @@ function toggleCheck13(){
   var x = document.getElementById("iframez13");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink13');
+  var z13 = document.getElementById("activezoom13");
   
   if(document.getElementById("myCheckbox13").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z13.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z13.style.color = "gray";
     
   }
 }
@@ -451,14 +483,17 @@ function toggleCheck14(){
   var x = document.getElementById("iframez14");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink14');
+  var z14 = document.getElementById("activezoom14");
   
   if(document.getElementById("myCheckbox14").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z14.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z14.style.color = "gray";
     
   }
 }
@@ -469,15 +504,17 @@ function toggleCheck15(){
   var x = document.getElementById("iframez15");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink15');
+  var z15 = document.getElementById("activezoom15");
   
   if(document.getElementById("myCheckbox15").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z15.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
-    
+    z15.style.color = "gray";
   }
 }
 
@@ -487,14 +524,17 @@ function toggleCheck16(){
   var x = document.getElementById("iframez16");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink16');
+  var z16 = document.getElementById("activezoom16");
   
   if(document.getElementById("myCheckbox16").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z16.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z16.style.color = "gray";
     
   }
 }
@@ -505,14 +545,17 @@ function toggleCheck17(){
   var x = document.getElementById("iframez17");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink17');
+  var z17 = document.getElementById("activezoom17");
   
   if(document.getElementById("myCheckbox17").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z17.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z17.style.color = "gray";
     
   }
 }
@@ -523,14 +566,17 @@ function toggleCheck18(){
   var x = document.getElementById("iframez18");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink18');
+  var z18 = document.getElementById("activezoom18");
   
   if(document.getElementById("myCheckbox18").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z18.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z18.style.color = "gray";
     
   }
 }
@@ -541,14 +587,17 @@ function toggleCheck19(){
   var x = document.getElementById("iframez19");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink19');
+  var z19 = document.getElementById("activezoom19");
   
   if(document.getElementById("myCheckbox19").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z19.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z19.style.color = "gray";
     
   }
 }
@@ -559,14 +608,17 @@ function toggleCheck20(){
   var x = document.getElementById("iframez20");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink20');
+  var z20 = document.getElementById("activezoom20");
   
   if(document.getElementById("myCheckbox20").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z20.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z20.style.color = "gray";
     
   }
 }
@@ -576,14 +628,17 @@ function toggleCheck21(){
   var x = document.getElementById("iframez21");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink21');
+  var z21 = document.getElementById("activezoom21");
   
   if(document.getElementById("myCheckbox21").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z21.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z21.style.color = "gray";
     
   }
 }
@@ -594,14 +649,17 @@ function toggleCheck22(){
   var x = document.getElementById("iframez22");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink22');
+  var z22 = document.getElementById("activezoom22");
   
   if(document.getElementById("myCheckbox22").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z22.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z22.style.color = "gray";
     
   }
 }
@@ -612,14 +670,17 @@ function toggleCheck23(){
   var x = document.getElementById("iframez23");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink23');
+  var z23 = document.getElementById("activezoom23");
   
   if(document.getElementById("myCheckbox23").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z23.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z23.style.color = "gray";
     
   }
 }
@@ -630,14 +691,17 @@ function toggleCheck24(){
   var x = document.getElementById("iframez24");
   var y = x.contentDocument;
   var z = x.contentDocument.getElementById('aLink24');
+  var z24 = document.getElementById("activezoom24");
   
   if(document.getElementById("myCheckbox24").checked === true){
       z.style.opacity = "1";
       z.style.height = "150";
+      z24.style.color = "#866ec7";
       }
    else {
     z.style.opacity = "0";
     z.style.height = "0";
+    z24.style.color = "gray";
     
   }
 }
